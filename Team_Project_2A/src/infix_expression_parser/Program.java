8180f47c317220d1727480e5a2f5c9e6d4ecf291
Kyle <98865889+kjmolitor@users.noreply.github.com>
2022-02-25 16:11:37 -0600

    Update Program.java

--- Team_Project_2A/src/infix_expression_parser/Program.java
+++ Team_Project_2A/src/infix_expression_parser/Program.java
@@ -1,12 +1,25 @@
 package infix_expression_parser;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/*Main program where we will read in data from input file
 		  and print output to console */
-
+		FileInputStream inputfile = new FileInputStream("input.txt");
+		Scanner scanner = new Scanner(inputfile);
+		Infix_Parser parser = new Infix_Parser();
+		
+		while(scanner.hasNext()) {
+			String exp = scanner.nextLine().replaceAll(" ", "");
+			parser.evaluate(exp);
+			
+		}
+		
+		inputfile.close();
+		scanner.close();
 	}//end main
 }//end Program
